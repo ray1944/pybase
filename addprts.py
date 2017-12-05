9bb25488cf55b4ee24dffe042210bfec9cfea437
pcheng <ray1944@gmail.com>
2017-12-05 16:35:17 +0800

    wraps running command function into partgen module

--- addprts.py
+++ addprts.py
@@ -1,14 +1,37 @@
 '''running ald command to add 50000 parts into remote lme server'''
 import partgen as pg
+import os
+import cmdsession
+
+curpath = os.getcwd()
+pg.chtodir()
+curpath1 = os.getcwd()
+# ald initialize
+pg.init()
+
+# ald signon
+pg.signon()
+
+# ald setdevpath
+pg.setdevpath()
+
+cs = cmdsession.Cmdsession(curpath)
+cs.serverids = pg.getSvrID()
+cs.savesess()
+cs.lastprtid = 34582
+
+# ald add parts and check in
+partprefix = 'partnm-'
+for num in range(cs.lastprtid, pg.numFile + 1):
+    filenm = partprefix + str(num).zfill(5)
+    cmd = pg.ald + ' add -i ' + filenm
+    ret = pg.runcmd(cmd)
+    if ret != 0:
+        print 'ald add ' + filenm + ' failed.'
+        cs.savesess()
+        break
+    else:
+        print 'adding ' + filenm + ' done'
 
 
-cmdpath = '/cygdrive/c/workspace/aldon/src/client_lme/aldcs/'
-# cmd = cmdpath + 'ald'
-cmd = 'ls -l'
-ret = pg.runcmd(cmd)
-
-if ret != 0:
-    print 'command ended with error'
-    exit(ret)
-else:
-    exit(0)
+exit(0)
