8ab9968795c9f78a759df762149d5d95227ba7ed
Pin Cheng <pcheng@rocketsoftware.com>
2017-12-15 11:15:54 +0800

    check out and then check in command

---
+++ chkoutin.py
@@ -0,0 +1,50 @@
+'''running ald command to add 50000 parts into remote lme server'''
+import partgen as pg
+import os
+import cmdsession
+
+curpath = os.getcwd()
+pg.chtodir()
+curpath1 = os.getcwd()
+# ald initialize
+pg.init()
+
+# ald signon
+pg.signon()
+
+# ald setdevpath
+#pg.setdevpath()
+
+cs = cmdsession.Cmdsession(curpath)
+cs.serverids = pg.getSvrID()
+cs.savesess()
+#cs.lastprtid = 1
+
+# ald add parts and check in
+partprefix = 'partnm-'
+for num in range(cs.lastprtid + 1, pg.numFile + 1):
+    filenm = partprefix + str(num).zfill(5)
+    cmd = pg.ald + ' checkout ' + filenm
+    ret = pg.runcmd(cmd)
+    if ret != 0:
+        print 'ald checkout ' + filenm + ' failed.'
+        cs.savesess()
+        break
+    else:
+        print 'checkout ' + filenm + ' done'
+        cs.lastprtid = num
+
+if cs.lastprtid == pg.numFile:
+    # check in
+    for num in range(1, pg.numFile + 1):
+        filenm = partprefix + str(num).zfill(5)
+        cmd = pg.ald + ' checkin ' + filenm
+        ret = pg.runcmd(cmd)
+        if ret != 0:
+            print 'ald checkin ' + filenm + ' failed. '
+            cs.savesesss()
+            break
+        else:
+            print 'checkin ' + filenm + ' done'
+
+exit(0)
