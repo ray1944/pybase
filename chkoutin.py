db6da47ea2adf723fe188b19025f17ac47955ac1
Pin Cheng <pcheng@rocketsoftware.com>
2017-12-18 17:34:34 +0800

    check in and out script

--- chkoutin.py
+++ chkoutin.py
@@ -2,6 +2,28 @@
 import partgen as pg
 import os
 import cmdsession
+import re
+
+def isCheckoutTwiceTime(cmdout):
+    ret = False
+    regstr = ('Checking out:.*', 'AFF1108.*')
+    conditions = []
+
+    for idx in range(0, len(regstr)):
+        conditions.append(False)
+
+    for out in cmdout:
+        for idx in range(0, len(regstr)):
+            pattern = re.compile(regstr[idx])
+            m = pattern.match(out)
+            if m is not None:
+                conditions[idx] = True
+
+    for con in conditions:
+        if not con:
+            return False
+
+    return True
 
 curpath = os.getcwd()
 pg.chtodir()
@@ -22,27 +44,32 @@ cs.savesess()
 
 # ald add parts and check in
 partprefix = 'partnm-'
-for num in range(cs.lastprtid + 1, pg.numFile + 1):
+for num in range(cs.lastchkoutprtid + 1, pg.numFile + 1):
     filenm = partprefix + str(num).zfill(5)
     cmd = pg.ald + ' checkout ' + filenm
-    ret = pg.runcmd(cmd)
+    consoleout = []
+    ret = pg.runcmd(cmd, consoleout)
     if ret != 0:
         print 'ald checkout ' + filenm + ' failed.'
-        cs.savesess()
+        #print consoleout[0]
+        if (isCheckoutTwiceTime(consoleout)):
+            continue
+        else:
+            cs.savesess()
         break
     else:
         print 'checkout ' + filenm + ' done'
         cs.lastprtid = num
 
-if cs.lastprtid == pg.numFile:
+if cs.lastchkoutprtid == pg.numFile:
     # check in
-    for num in range(1, pg.numFile + 1):
+    for num in range(cs.lastchkinprtid, pg.numFile + 1):
         filenm = partprefix + str(num).zfill(5)
         cmd = pg.ald + ' checkin ' + filenm
         ret = pg.runcmd(cmd)
         if ret != 0:
             print 'ald checkin ' + filenm + ' failed. '
-            cs.savesesss()
+            cs.savesess()
             break
         else:
             print 'checkin ' + filenm + ' done'
