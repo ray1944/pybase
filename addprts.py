6ab55a9c2f93c5aae383b8b1a1bfe1e9bff77493
Pin Cheng <pcheng@dln-vm-aldon08.rocketsoftware.com>
2017-12-27 17:35:55 -0800

    use CommandProcess instead

--- addprts.py
+++ addprts.py
@@ -1,37 +1,28 @@
 '''running ald command to add 50000 parts into remote lme server'''
-import partgen as pg
+from CommandProcess import CommandProcess as CP
 import os
 import cmdsession
 
-curpath = os.getcwd()
+# cs = cmdsession.Cmdsession(curpath)
+pg = CP()
+cs = pg.getSession()
 pg.chtodir()
-curpath1 = os.getcwd()
+
 # ald initialize
-pg.init()
+#pg.init()
 
 # ald signon
 pg.signon()
 
 # ald setdevpath
-pg.setdevpath()
+# pg.setdevpath()
 
-cs = cmdsession.Cmdsession(curpath)
+
 cs.serverids = pg.getSvrID()
 cs.savesess()
-cs.lastprtid = 34582
 
 # ald add parts and check in
-partprefix = 'partnm-'
-for num in range(cs.lastprtid, pg.numFile + 1):
-    filenm = partprefix + str(num).zfill(5)
-    cmd = pg.ald + ' add -i ' + filenm
-    ret = pg.runcmd(cmd)
-    if ret != 0:
-        print 'ald add ' + filenm + ' failed.'
-        cs.savesess()
-        break
-    else:
-        print 'adding ' + filenm + ' done'
+pg.addparts(49999)
 
 
 exit(0)
